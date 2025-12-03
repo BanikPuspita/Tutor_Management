0194921e1e0c8b3e4c4cd775f1ac4ff6bac59f6a
puspita <puspitabanik213@gmail.com>
2025-12-03 20:26:49 +0600

    new update

--- app/routes.ts
+++ app/routes.ts
@@ -1,8 +1,8 @@
 // app/routes.ts
 import { type RouteConfig, layout } from "@react-router/dev/routes";
-import { authRoutes } from "./routes/auth.routes";
+//import { authRoutes } from "./routes/auth.routes";
 import { publicRoutes } from "./routes/public.routes";
-import { privateRoutes } from "./routes/private.routes";
+//import { privateRoutes } from "./routes/private.routes";
 
 export default [
   // layout("pages/auth/layout.tsx", authRoutes),
